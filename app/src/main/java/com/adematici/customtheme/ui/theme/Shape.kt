ee06444fa0117f617007de91af506fb1619f5cfb
adematici <adematicis41@gmail.com>
2022-01-10 00:11:44 +0300

    Create shapes palette

--- app/src/main/java/com/adematici/customtheme/ui/theme/Shape.kt
+++ app/src/main/java/com/adematici/customtheme/ui/theme/Shape.kt
@@ -2,6 +2,7 @@ package com.adematici.customtheme.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
@@ -9,3 +10,17 @@ val Shapes = Shapes(
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
 )
+
+data class ShapesPalette(
+    val small: Shapes = Shapes(RoundedCornerShape(6.dp)),
+    val medium: Shapes = Shapes(RoundedCornerShape(12.dp)),
+    val large: Shapes = Shapes(RoundedCornerShape(18.dp)),
+    val buttonShape: Shapes = Shapes(
+        RoundedCornerShape(
+            topStart = 20.dp,
+            bottomStart = 20.dp
+        )
+    ),
+)
+
+internal val LocalShapes = staticCompositionLocalOf { ShapesPalette() }
